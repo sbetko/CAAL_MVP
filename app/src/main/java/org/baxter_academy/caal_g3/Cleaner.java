af693133d02f40a4591ca0a6aa5879b39497bf88
pkla <lyanauz@gmail.com>
2016-12-09 14:00:58 -0500

    Initial commit

---
+++ app/src/main/java/org/baxter_academy/caal_g3/Cleaner.java
@@ -0,0 +1,8 @@
+package org.baxter_academy.caal_g3;
+
+/**
+ * Created by Baxter on 12/9/2016.
+ */
+
+public class Cleaner {
+}
---
+++ app/src/main/java/org/baxter_academy/caal_g3/Reader.java
@@ -0,0 +1,12 @@
+package org.baxter_academy.caal_g3;
+
+import android.app.IntentService;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Baxter on 12/2/2016.
+ */
+
+public class Reader extends IntentService {
+
+}
