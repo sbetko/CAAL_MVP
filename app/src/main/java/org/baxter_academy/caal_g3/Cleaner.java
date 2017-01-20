e9d1206691d827f7632ec5648bfe3c56964e78fb
pkla <lyanauz@gmail.com>
2017-01-20 10:40:03 -0500

    updated cleaner output

--- app/src/main/java/org/baxter_academy/caal_g3/Cleaner.java
+++ app/src/main/java/org/baxter_academy/caal_g3/Cleaner.java
@@ -15,6 +15,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 //TODO write pre-processing / ARFF formatting functions
+/** Cleaner pre-processes data before it is classified **/
 public class Cleaner extends Service {
     public void onCreate() {
         String arffDataFilename = "processedData";
@@ -29,19 +30,7 @@ public class Cleaner extends Service {
         }
 
         StringBuilder toWrite = new StringBuilder();
-        toWrite.append("@relation activity_recognition_labeled");
-        toWrite.append("'@attribute \"X8\" numeric");
-        toWrite.append("@attribute \"Z3\" numeric");
-        toWrite.append("@attribute \"ZAVG\" numeric");
-        toWrite.append("@attribute \"XPEAK\" numeric");
-        toWrite.append("@attribute \"ZPEAK\" numeric");
-        toWrite.append("@attribute \"YSTANDDEV\" numeric");
-        toWrite.append("@attribute class{ \"Sitting\" , \"Active\" }");
-        toWrite.append("@data");
-        toWrite.append("0.13,0.12,1.76,2075,1550,8.17,?");
-        toWrite.append("0,0,8.24,257.35,315,0.03,?");
 
-/** NEWLINE MAY NOT BE NEEDED WITH STRINGBUILDER.APPEND()
         toWrite.append("@relation activity_recognition_labeled" + System.getProperty("line.separator"));
         toWrite.append("'@attribute \"X8\" numeric" + System.getProperty("line.separator"));
         toWrite.append("@attribute \"Z3\" numeric"  + System.getProperty("line.separator"));
@@ -53,7 +42,7 @@ public class Cleaner extends Service {
         toWrite.append("@data");
         toWrite.append("0.13,0.12,1.76,2075,1550,8.17,?" + System.getProperty("line.separator"));
         toWrite.append("0,0,8.24,257.35,315,0.03,?" + System.getProperty("line.separator"));
-**/
+
         String finalString = toWrite.toString();
 
         try {
