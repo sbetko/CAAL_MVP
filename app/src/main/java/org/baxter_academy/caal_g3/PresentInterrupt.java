f42ca17f7e28aca588b9bca7feb1de2b2d0c6892
pkla <lyanauz@gmail.com>
2017-05-12 15:58:39 -0400

    working interrupt logic

--- app/src/main/java/org/baxter_academy/caal_g3/PresentInterrupt.java
+++ app/src/main/java/org/baxter_academy/caal_g3/PresentInterrupt.java
@@ -56,6 +56,7 @@ public class PresentInterrupt extends Service {
     public BufferedWriter writer = null;
     public BufferedWriter sitWriter = null;
     public String activity = null;
+    public boolean noActivity = false;
 
     public long startSitTime;
     public long cTime;
@@ -71,130 +72,105 @@ public class PresentInterrupt extends Service {
 
         /** loads activity from WekaClassifier file output **/
         try {
-
             BufferedReader reader = new BufferedReader(new FileReader(file)); //fixme reference to local declaration in WekaClassifier
 
-            for(int i = 1; i < 51; i++) {
+            for (int i = 1; i < 51; i++) {
                 reader.readLine();
             }
 
             String actLine = reader.readLine();
-            String actLineSplit [] = actLine.split(",");
+            String actLineSplit[] = actLine.split(",");
 
             /** If there's a classification, then proceed, otherwise restart (this is due to a BUG) **/
-            if (Objects.equals(actLine, "") || (actLine.isEmpty())) {
-                // make call to META that core service chain needs restarting
-                Log.d("sender", "Broadcasting message");
-                Intent intent = new Intent("Error");
-                // You can also include some extra data.
-                intent.putExtra("solution", "Restart immediately");
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-                stopSelf();
-            }
-
-            try {
+            if (!Objects.equals(actLine, "") || !(actLine.isEmpty())) {
+                noActivity = false;
                 String activity = actLineSplit[45]; // pulls out specific name of activity
-            } catch (ArrayIndexOutOfBoundsException e) {
-                Intent intent = new Intent("Error");
-                // You can also include some extra data.
-                intent.putExtra("solution", "Restart immediately");
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-                stopSelf();
-            }
+                reader.close();
 
-            String activity = actLineSplit[45]; // pulls out specific name of activity
+                /** logs activity with timestamp **/
+                //fixme CRITICAL logfile not written over on restart of coreServiceChain
+                //fixme this means that on a resume of the service from the app GUI or after a crash,
+                //fixme reminder logic will be broken
 
-            reader.close();
+                //File logfile = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), meta.activityLogFilename);
+                // General activity logging
+                writer = new BufferedWriter(new FileWriter(
+                        new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "activityLog"), true)); //fixme hardcoded log file name
+                writer.write(System.currentTimeMillis() + "," + activity);
+                writer.newLine();
+                writer.flush();
+                writer.close();
+                System.out.println(System.currentTimeMillis() + "," + activity); //debug
 
-            /** logs activity with timestamp **/
-            //fixme CRITICAL logfile not written over on restart of coreServiceChain
-            //fixme this means that on a resume of the service from the app GUI or after a crash,
-            //fixme reminder logic will be broken
-
-            //File logfile = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), meta.activityLogFilename);
-            // General activity logging
-            writer = new BufferedWriter(new FileWriter(
-                    new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "activityLog"), true)); //fixme hardcoded log file name
-            writer.write(System.currentTimeMillis() + "," + activity);
-            writer.newLine();
-            writer.flush();
-            writer.close();
-            System.out.println(System.currentTimeMillis() + "," + activity); //debug
-
-            /** Sitting-only logging (for interrupt logic, strictly for easier implementation) **/
-            // creates new text file
-            String actString = activity.toString(); //need to convert, or else .equals won't work
-            String testString = "Sitting";
-            System.out.println(actString + " = " + "Sitting");
-
-            if (!actString.equals(testString)) {
-                System.out.println(actString + " != " + "Sitting");
-                new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "sittingLog"); //RESET LOG
-            } else { //if (Objects.equals(activity, "Sitting")) {
+                /** Sitting-only logging (for interrupt logic, strictly for easier implementation) **/
+                // creates new text file
+                String actString = activity.toString(); //need to convert, or else .equals won't work
+                String testString = "Sitting";
                 System.out.println(actString + " = " + "Sitting");
 
-                sitWriter = new BufferedWriter(new FileWriter(
-                        new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "sittingLog"), true
-                ));
-                sitWriter.write(System.currentTimeMillis() + "," + activity);
-                sitWriter.newLine();
-                sitWriter.flush();
-                sitWriter.close();
+                if (!actString.equals(testString)) {
+                    System.out.println(actString + " != " + "Sitting");
+                    new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "sittingLog"); //RESET LOG
+                } else { //if (Objects.equals(activity, "Sitting")) {
+                    System.out.println(actString + " = " + "Sitting");
 
-                /** begins interrupt logic **/
-                BufferedReader sitReader = new BufferedReader(
-                        new FileReader(
-                                new File(getExternalFilesDir(
-                                        Environment.DIRECTORY_DOCUMENTS), "sittingLog")
-                        )); //fixme hardcoded log file name
+                    sitWriter = new BufferedWriter(new FileWriter(
+                            new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "sittingLog"), true
+                    ));
+                    sitWriter.write(System.currentTimeMillis() + "," + activity);
+                    sitWriter.newLine();
+                    sitWriter.flush();
+                    sitWriter.close();
 
-                int iter = 0;
-                while (sitReader.readLine() != null) {
-                    if (iter == 0) {
-                        startSitTime = Long.parseLong(actLineSplit[1]);
+                    /** begins interrupt logic **/
+                    BufferedReader sitReader = new BufferedReader(
+                            new FileReader(
+                                    new File(getExternalFilesDir(
+                                            Environment.DIRECTORY_DOCUMENTS), "sittingLog")
+                            )); //fixme hardcoded log file name
+
+                    int iter = 0;
+                    while (sitReader.readLine() != null) {
+                        if (iter == 0) {
+                            startSitTime = Long.parseLong(actLineSplit[1]);
+                        }
+                        if (iter > 0) {
+                            sitDuration = System.currentTimeMillis() - startSitTime;
+                        }
+                        iter++;
                     }
-                    if (iter > 0) {
-                        sitDuration = System.currentTimeMillis() - startSitTime;
-                    }
-                    iter++;
                 }
+
+                /** Check if its time to remind the user **/
+                if (sitDuration > maxSitTime) {
+                    //it's time, send notification
+                    System.out.println("Sent Notification");
+                    //also, write over sittingLog
+                }
+                stopSelf();
+            } else {
+                // make call to META that core service chain needs restarting
+                noActivity = true;
+                System.out.println("Restarted");
+                Intent intent = new Intent("Error");
+                intent.putExtra("solution", "Restart immediately");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+                stopSelf();
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        /** Check if its time to remind the user **/
-        if (sitDuration > maxSitTime) {
-            //it's time, send notification
-            NotificationCompat.Builder mBuilder =
-                    (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                    //.setSmallIcon()
-                    .setContentTitle("Take a break!")
-                    .setContentTitle("It's time to stand");
-
-            Intent resultIntent = new Intent(this, PresentInterrupt.class);
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-            stackBuilder.addParentStack(PresentInterrupt.class);
-            stackBuilder.addNextIntent(resultIntent);
-            PendingIntent resultPendingIntent =
-                    stackBuilder.getPendingIntent(
-                            0,
-                            PendingIntent.FLAG_UPDATE_CURRENT
-                    );
-            mBuilder.setContentIntent(resultPendingIntent);
-            NotificationManager mNotificationManager =
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            mNotificationManager.notify(1, mBuilder.build()); // int 1 is notification ID
-        }
-        stopSelf();
     }
 
     public void onDestroy() {
-        Log.d("sender", "Broadcasting message");
-        Intent intent = new Intent("FinishedWork");
-        // You can also include some extra data.
-        intent.putExtra("status", "PresentInterrupt finished");
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        if (!noActivity) {
+            Log.d("sender", "Broadcasting message");
+            Intent intent = new Intent("FinishedWork");
+            // You can also include some extra data.
+            intent.putExtra("status", "PresentInterrupt finished");
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        }
     }
 
     @Nullable
